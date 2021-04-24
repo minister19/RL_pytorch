667e0331ab4da8c0756adb25a876b9ffa1ebd758
Shawn <gaoshuang.dalian@gmail.com>
2021-04-25 00:40:41 +0800

    Test 1d maze with Q Learning and Sarsa.

--- maze_1d_example.py
+++ maze_1d_example.py
@@ -1,18 +1,19 @@
 import math
 import torch
 from rl_m19.config import Config
-from rl_m19.agent import SarsaAgent
+from rl_m19.agent import QLearningAgent, SarsaAgent
 from rl_m19.envs import OneDimensionMaze
 from rl_m19.network import ReplayMemory, PureLinear
 from rl_m19.utils import Plotter
 
 config = Config()
-config.episode_lifespan = 500
+config.episode_lifespan = 1000
 config.episodes = 1000
 config.BATCH_SIZE = 64
 config.GAMMA = 0.999
+# config.EPS_fn = lambda s: 0.9
 config.EPS_fn = lambda s: 0.05 + (0.9 - 0.05) * math.exp(-1. * s / 200)
-config.LR = 0.001  # LEARNING_RATE
+config.LR = 0.1  # LEARNING_RATE
 config.MC = 1000  # MEMORY_CAPACITY
 config.TUF = 10  # TARGET_UPDATE_FREQUENCY
 
@@ -28,6 +29,7 @@ config.target_net_fn = lambda: PureLinear(config)
 config.optimizer_fn = torch.optim.RMSprop
 config.loss_fn = torch.nn.MSELoss()
 
+# agent = QLearningAgent(config)
 agent = SarsaAgent(config)
 agent.episodes_learn()
 config.env.render()
