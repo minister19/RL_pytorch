33224f7fc724b2a380f97d59f3715de58e65f6a3
Shawn <gaoshuang.dalian@gmail.com>
2021-04-25 00:40:02 +0800

    Base Sarsa on Q Learning.

--- RL_FRAMEWORK/rl_m19/agent/sarsa_agent.py
+++ RL_FRAMEWORK/rl_m19/agent/sarsa_agent.py
@@ -1,66 +1,33 @@
 import numpy as np
 import pandas as pd
 import random
+import time
 
 from itertools import count
-from .base_agent import BaseAgent
+from .q_learning_agent import QLearningAgent
 
 
-class SarsaAgent(BaseAgent):
+class SarsaAgent(QLearningAgent):
     def __init__(self, config):
         super().__init__(config)
         self.actions = range(self.config.actions_dim)
         self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float32)
         self.select_action_counter = 0
 
-    def __check_state_exist(self, state):
-        if state not in self.q_table.index:
-            # append new state to q table
-            new_state_row = pd.Series(
-                np.zeros(self.config.actions_dim),
-                index=self.actions,
-                name=state,
-            )
-            self.q_table = self.q_table.append(new_state_row)
-
-    def select_action(self, state):
+    def learn(self, state, action, reward, next_state, next_action):
         # convert tensor to string, number
         state = self.tensor2str(state.cpu())
-
-        # record unknown state
-        self.__check_state_exist(state)
-
-        sample = random.random()
-        eps = self.eps_fn(self.select_action_counter)
-        self.select_action_counter += 1
-        if sample < eps:
-            # some actions may have the same value, randomly choose on in these actions
-            state_action = self.q_table.loc[state, :]
-            best_actions = state_action[state_action == max(state_action)].index
-            action = random.choice(best_actions)
-        else:
-            action = random.choice(self.actions)
-        return action
-
-    def learn(self, state, action, reward, next_state):
-        # convert tensor to string, number
-        state = self.tensor2str(state.cpu())
-        real_reward = self.tensor2number(reward.cpu())
-        if real_reward > 0:
-            reward = 1
-        elif real_reward == 0:
-            reward = 0
-        else:
-            reward = -1
+        reward = self.tensor2number(reward.cpu())
+        reward = self._unify_reward(reward)
         next_state = self.tensor2str(next_state.cpu())
 
         # record unknown state
-        self.__check_state_exist(next_state)
+        self._check_state_exist(next_state)
 
         q_predict = self.q_table.loc[state, action]
         if next_state != None:
             # next state is not terminal
-            q_target = reward + self.config.GAMMA * self.q_table.loc[next_state, :].max()
+            q_target = reward + self.config.GAMMA * self.q_table.loc[next_state, next_action].max()
         else:
             # next state is terminal
             q_target = reward
@@ -70,25 +37,28 @@ class SarsaAgent(BaseAgent):
         state = self.config.env.reset()
 
         for t in count():
-            # self.config.env.render()
+            self.config.env.render()
+            time.sleep(0.05)
 
-            # take action, observe
+            # choose action
             action = self.select_action(state)
+
+            # take action and observe
             next_state, reward, done, info = self.config.env.step(action)
 
+            # choose next_action
+            next_action = self.select_action(state)
+
             if done or t >= self.config.episode_lifespan:
-                # print(self.q_table)
                 self.config.env.render()
+                print(f'\r\n{self.q_table}', end='\r\n')
+
                 self.episode_t.append(t)
                 # self.config.plotter.plot_list_ndarray(self.episode_t)
                 break
             else:
                 # learn
-                self.learn(state, action, reward, next_state)
+                self.learn(state, action, reward, next_state, next_action)
 
                 # update state
                 state = next_state
-
-    def episodes_learn(self):
-        for i_episode in range(self.config.episodes):
-            self.episode_learn(i_episode)
