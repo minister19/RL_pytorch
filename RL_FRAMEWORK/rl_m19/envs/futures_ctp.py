d67376f5390e23258fb482b0b6c2491d7d8f7aa9
Shawn <gaoshuang.dalian@gmail.com>
2021-05-06 23:25:18 +0800

    Remove old draft.

--- RL_FRAMEWORK/rl_m19/envs/futures_ctp.py
+++ RL_FRAMEWORK/rl_m19/envs/futures_ctp.py
@@ -1,142 +1,124 @@
+import math
 import numpy as np
-import matplotlib.pyplot as plt
+import numpy.random as ran
 
-from enum import Enum
 from .base_env import BaseEnv
-from .futures.account import Account
-from .futures.frequency import Frequency
 
 '''
-Observations:
-0   price, i.e. bar.close
-1   volume
-    1. original data
-    2. 0, aims to be ignored by network
-2   qianlon
-3   qianlon_ma
-4   rsi_ma
-5   rsi_smh_ma
-
-Actions v1:
-0   Wait
-    平仓观察
-1   Positive_1
-2   Positive_2
-3   Positive_3
-    立即持多仓
-4   Negative_1
-5   Negative_2
-6   Negative_3
-    立即持空仓
-
-Actions v2:
-0   Wait
-    平仓观察
-1   Unilateral
-    单边，boll 15%置信区间开仓
-2   Oscillated
-    振荡，boll 95%置信区间开仓
-
+Description:
+    Future CTP is a trading account for trader to open/close short/long positions.
+Source:
+    Shuang Gao
+Observation:
+    Type: Box(10)
+    Num     Obersvation     Min     Max     Discrete
+    0       Fund            -inf    inf
+    1       Position                        -1.0/-0.5/0/0.5/1.0
+    2       Margin (%)      -inf    inf
+    3       Emas                            -1/0/1
+    4       Mas                             -1/0/1
+    5       Qianlon lon                     -1/0/1
+    6       Qianlon vel                     -1/0/1
+    7       Boll sig                        -4/-3/-2/0/2/3/4
+    8       Period sig                      -2/-1/0/1/2
+    9       RSI sig                         -2/-1/0/1/2
+Actions:
+    Type: Discrete(6)
+    Num   Action
+    0     Open long 0.5
+    1     Open long 1.0
+    2     Close long
+    3     Open short 0.5
+    4     Open short 1.0
+    5     Close short
 Reward:
-Change in decimal (rather than %) of account's fund.
-
-Starting state:
-Ignore the first n points (due to Exponential Moving Average, Simple Moving Average, etc.)
-
-Episode termination:
-1. freq's data stream out.
-2. account's fund goes below 50%, i.e. reward <= -0.5.
+    Type: Discrete(3)
+    Reward      Reason
+    0           abs(Margin) <= 1.0
+    -inf        Margin < 1.0
+    inf         Margin > 1.0
+Starting State:
+    Fund = 10K
+    Steps >= 100 (ignore EMA, SMA's beginning values)
+Episode Termination:
+    Fun <= 5K
+    Steps >= len(history data)
 '''
+
+# TODO: research action Close short/long 0.5
+# TODO: research when should qianlon lon/vel ~= 0
 
 
 class FuturesCTP(BaseEnv):
-    def __init__(self, device, account, freq):
+    def __init__(self, device) -> None:
         super().__init__(device)
-        self.account = account
-        self.freq = freq
-        self.states_dim = 4
-        self.actions_dim = 7
-        self.step_to_run = len(freq.data.open) - 1
-        self.step_index = None
+        self.states_dim = self.maze_length**2
+        self.actions_dim = 4  # move left/right/up/down
+        self.steps = None
+
+    @property
+    def trap(self): return [6, 6]
+
+    @property
+    def terminal(self): return [self.maze_length-1, self.maze_length-1]
 
     def __get_state(self):
-        i = self.step_index
-        d = self.freq.data
-        # price = d.close[i]
-        # volume = d.volume[i]
-        # ql = d.qianlon.lon[i]
-        # ql_ma = d.qianlon.lon_ma[i]
-        # rsi_ma = d.rsi.rsi_ma[i]
-        # rsi_smh_ma = d.rsi.rsi_smh_ma[i]
-        # return [price, volume, ql, ql_ma, rsi_ma, rsi_smh_ma]
+        return [self.posi_x, self.posi_y]
 
-        d1 = d.bbands.location[i]
-        d2 = d.period.period[i]
-        d3 = d.qianlon.region[i]
-        d4 = d.qianlon.ma_isup[i]
-        return [d1, d2, d3, d4]
+    def step(self, action: int):
+        self.steps += 1
 
-    def __get_current_price(self):
-        i = self.step_index
-        d = self.freq.data
-        price = d.close[i]
-        return price
+        # 1. snapshot state
+        state = self.__get_state()
 
-    def step(self, action):
-        # 1. take action
-        self.account.take_action_v1(action)
-        # self.account.take_action_v2(action, self.freq.data)
+        # 2. take action
+        if action == 0:
+            self.posi_x = max(0, self.posi_x - 1)
+        elif action == 1:
+            self.posi_x = min(self.maze_length-1, self.posi_x + 1)
+        elif action == 2:
+            self.posi_y = max(0, self.posi_y - 1)
+        elif action == 3:
+            self.posi_y = min(self.maze_length-1, self.posi_y + 1)
 
-        # 2. get next state
-        self.step_index += 1
+        # 3. get next state
         next_state = self.__get_state()
 
-        # 3. update reward basing on next state
-        reward = self.account.update_reward(self.__get_current_price()) / self.account.fund * 100
+        # 4. update reward basing on next state
+        if next_state == self.trap:
+            reward = -10
+        elif next_state == state:
+            reward = -5
+        elif next_state == self.terminal:
+            reward = 100
+        else:
+            reward = -1
 
-        # 4. test if done
-        if self.step_index >= self.step_to_run:
-            done = True
-        elif self.account.fund < 0.50*self.account.init_fund:
+        # 5. test if done
+        if next_state == self.terminal or self.steps >= 1000:
             done = True
         else:
             done = False
 
-        # 5. currently, info is None
+        # 6. currently, info is None
         info = None
 
         return self._unsqueeze_tensor(next_state), self._unsqueeze_tensor(reward), done, info
 
     def reset(self):
-        self.step_index = self.freq.data.ignore_len  # Ignore the first n points
-        self.account.reset(self.__get_current_price())
+        self.posi_x = ran.randint(self.maze_length)
+        self.posi_y = ran.randint(self.maze_length)
+        self.steps = 0
         state = self.__get_state()
         return self._unsqueeze_tensor(state)
 
     def render(self):
-        close = self.freq.data.close[self.freq.data.ignore_len:self.step_index]
-        reward = self.account.reward
-
-        fig1 = plt.figure(1)
-        plt.clf()
-        ax1 = fig1.add_subplot(111)
-
-        # 2020-08-20 Shawn: TODO: 打印开平仓标志，便于分析持仓时间。
-        color = 'tab:red'
-        ax1.set_xlabel('Time (' + self.freq.freq + ')')
-        ax1.set_ylabel('Close', color=color)
-        ax1.plot(close, color=color)
-        ax1.tick_params(axis='y', labelcolor=color)
-
-        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-        color = 'tab:blue'
-        ax2.set_ylabel('Reward', color=color)  # we already handled the x-label with ax1
-        ax2.plot(reward, color=color)
-        ax2.tick_params(axis='y', labelcolor=color)
-
-        fig1.tight_layout()  # otherwise the right y-label is slightly clipped
-        plt.pause(0.001)
+        maze = '.' * self.maze_length**2
+        posi = self.trap[0]*self.maze_length + self.trap[1]
+        maze = maze[0:posi] + 'o' + maze[posi+1:]
+        posi = self.posi_x*self.maze_length + self.posi_y
+        maze = maze[0:posi] + 'x' + maze[posi+1:]
+        print(maze)
 
     def close(self):
-        state = np.zeros(self.states_dim)
-        return self._unsqueeze_tensor(state)
+        pass
