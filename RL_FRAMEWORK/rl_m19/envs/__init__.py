18ac48cc91e2ab2f9089d3ac7130937acbcd126e
Shawn <gaoshuang.dalian@gmail.com>
2021-07-22 22:47:39 +0800

    Refactor env.

--- RL_FRAMEWORK/rl_m19/envs/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base_env import BaseEnv
-from .cart_pole_v0 import CartPole_v0
-from .maze_1d import OneDimensionMaze
-from .maze_2d import TwoDimensionMaze
-from .futures import FuturesCTP
--- RL_FRAMEWORK/rl_m19/envs/futures/__init__.py
+++
@@ -1,1 +0,0 @@
-from .futures_ctp import FuturesCTP
