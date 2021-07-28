e20463802f447129c7d65990e2010f5722693371
Shawn <gaoshuang.dalian@gmail.com>
2021-07-28 22:02:07 +0800

    Update sec 1.

--- 1_intro/6_numpy_1.py
+++ 1_intro/6_numpy_1.py
@@ -1,4 +1,6 @@
+import torch
 import numpy as np
+
 A = np.array([1, 2, 3])
 B = np.array([4, 5, 6])
 
@@ -11,3 +13,12 @@ try:
 except Exception as e:
     print(e)
 print(A.dot(C))
+
+x = torch.tensor([[1], [2]])
+print(x)
+print(x[:, 0])
+
+x = torch.tensor([[[1], [2]], [[1], [2]]])
+print(x)
+print(x[:, 0])
+print(x[:, 1])
