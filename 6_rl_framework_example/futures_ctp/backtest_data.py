791b4ec57b12d2774aaad0e875bb69683ad0d91e
Shawn <gaoshuang.dalian@gmail.com>
2021-08-08 21:01:55 +0800

    Stage backtest_data.

--- 6_rl_framework_example/futures_ctp/backtest_data.py
+++ 6_rl_framework_example/futures_ctp/backtest_data.py
@@ -1,85 +1,46 @@
 import asyncio
 import json
-from numpy import sign
+import numpy as np
 from websockets import client
 
 
 class SingleIndicator:
     def __init__(self) -> None:
         self.value = []
-        self.feedback_await_reset = False
+        self.feedback_sign = None
         self.feedback_cost = None
 
     def forward(self, i, close, new_cost):
-        val = self.value[i]
-        if val == 0:
-            if self.feedback_cost:
-                feedback = (close - self.feedback_cost) * sign(val)
+        s = np.sign(self.value[i])
+        if self.feedback_sign:
+            if s == 0 or s == self.feedback_sign:
+                feedback = round((close - self.feedback_cost) / self.feedback_cost, 3)
             else:
+                self.feedback_sign = s
+                self.feedback_cost = new_cost
                 feedback = 0
-            self.feedback_await_reset = True
         else:
-            if self.feedback_await_reset:
-                feedback = 0
-                self.feedback_await_reset = False
+            if s != 0:
+                self.feedback_sign = s
                 self.feedback_cost = new_cost
-            elif self.feedback_cost:
-                feedback = (close - self.feedback_cost) * sign(val)
-            else:
-                feedback = 0
-                self.feedback_cost = new_cost
+            feedback = 0
         return feedback
-
-
-class Indicators:
-    def __init__(self) -> None:
-        self.i = 0
-        self.emas_sign = SingleIndicator()
-        self.emas_support = SingleIndicator()
-        self.qianlon_sign = SingleIndicator()
-        self.qianlon_trend = SingleIndicator()
-        self.qianlon_vel = SingleIndicator()
-        self.boll_sig = SingleIndicator()
-        self.period_sig = SingleIndicator()
-        self.rsi_sig = SingleIndicator()
-        self.withdraw = SingleIndicator()
-
-    def forward(self, kline):
-        if self.i >= len(self.emas_sign):
-            raise RuntimeError('Indicator data are exhausted.')
-        else:
-            ret = []
-            for indic in self.indicators:
-                ret.append(indic.value[self.i])
-                if indic == self.withdraw:
-                    if indic.value[self.i] == -1:
-                        new_cost = kline.low
-                    elif indic.value[self.i] == 1:
-                        new_cost = kline.high
-                else:
-                    new_cost = kline.close
-                    ret.append(indic.forward(self.i, kline.close, new_cost))
-            self.i += 1
-            return ret
-
-    @property
-    def indicators(self):
-        return (self.emas_sign,
-                self.emas_support,
-                self.qianlon_sign,
-                self.qianlon_trend,
-                self.qianlon_vel,
-                self.boll_sig,
-                self.period_sig,
-                self.rsi_sig,
-                self.withdraw
-                )
 
 
 class BacktestData:
     def __init__(self) -> None:
+        self.i = 0
         self.klines = []
-        self.indicators = Indicators()
+        self.emas_trend = SingleIndicator()
+        self.emas_support = SingleIndicator()
+        self.qianlon_sign = SingleIndicator()
+        self.qianlon_trend = SingleIndicator()
+        self.qianlon_vel_sign = SingleIndicator()
+        self.boll_sig = SingleIndicator()
+        self.period_sig = SingleIndicator()
+        self.rsi_sig = SingleIndicator()
+        self.withdraw = SingleIndicator()
+        self.state = [0] * (1+len(self.indicators)*2)
 
     async def sync(self):
         '''
@@ -95,9 +56,89 @@ class BacktestData:
             msg2 = await websocket.recv()
             data2 = json.loads(msg2)
             ema_3 = data2['data']['emas']['ema_3']
+            ema_3_support = data2['data']['emas']['ema_3_support']
+            lon = data2['data']['qianlon']['lon']
+            lon_vel = data2['data']['qianlon']['lon_vel']
+            boll_sig = data2['data']['boll_aux']['sig']
+            period_sig = data2['data']['period']['sig']
+            rsi_sig = data2['data']['rsi_smh']['sig']
+            withdraw = data2['data']['withdraw']['sig']
+
+            for i in range(len(ema_3)):
+                if i == 0 or ema_3[i-1] == ema_3[i]:
+                    trend = 0
+                elif ema_3[i-1] < ema_3[i]:
+                    trend = 1
+                else:
+                    trend = -1
+                self.emas_trend.value.append(trend)
+
+                self.qianlon_sign.value.append(np.sign(lon[i]))
+                if i == 0 or lon[i-1] == lon[i]:
+                    trend = 0
+                elif lon[i-1] < lon[i]:
+                    trend = 1
+                else:
+                    trend = -1
+                self.qianlon_trend.value.append(trend)
+                self.qianlon_vel_sign.value.append(np.sign(lon_vel[i]))
+            self.emas_support.value = ema_3_support
+            self.boll_sig.value = boll_sig
+            self.period_sig.value = period_sig
+            self.rsi_sig.value = rsi_sig
+            self.withdraw.value = withdraw
+
+    def reset(self):
+        self.i = 0
+        self.state = [0] * (1+len(self.indicators))
+
+    def forward(self):
+        if self.i >= len(self.klines):
+            raise RuntimeError('Indicator data are exhausted.')
+        else:
+            kline = self.klines[self.i]
+            self.state.clear()
+            self.state.append(kline['close'])
+            for indic in self.indicators:
+                val = indic.value[self.i]
+                self.state.append(val)
+                if indic == self.withdraw:
+                    if val == -1:
+                        new_cost = kline['low']
+                    elif val == 1:
+                        new_cost = kline['high']
+                else:
+                    new_cost = kline['close']
+                self.state.append(indic.forward(self.i, kline['close'], new_cost))
+            self.i += 1
+
+    @property
+    def indicators(self):
+        return (self.emas_trend,
+                self.emas_support,
+                self.qianlon_sign,
+                self.qianlon_trend,
+                self.qianlon_vel_sign,
+                self.boll_sig,
+                self.period_sig,
+                self.rsi_sig,
+                self.withdraw
+                )
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
     bd = BacktestData()
+    loop = asyncio.get_event_loop()
     loop.run_until_complete(bd.sync())
+    for i in range(len(bd.klines)):
+        bd.forward()
+        x = bd.state[1:]
+        print(f'e_trend {x[0]}, {x[1]:.3f}', end='\t')
+        print(f'e_support {x[2]}, {x[3]:.3f}', end='\t')
+        print(f'q_sign {x[4]}, {x[5]:.3f}', end='\t')
+        print(f'q_trend {x[6]}, {x[7]:.3f}', end='\t')
+        print(f'q_vel_sign {x[8]}, {x[9]:.3f}', end='\t')
+        print(f'boll_sig {x[10]}, {x[11]:.3f}', end='\t')
+        print(f'period_sig {x[12]}, {x[13]:.3f}', end='\t')
+        print(f'rsi_sig {x[14]}, {x[15]:.3f}', end='\t')
+        print(f'withdraw {x[16]}, {x[17]:.3f}')
