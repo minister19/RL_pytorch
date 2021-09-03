8dbb6b6eb22a3568f93ceb1c73fd23c5a0eebeb8
Shawn <gaoshuang.dalian@gmail.com>
2021-09-03 16:24:18 +0800

    Support logger.

--- 6_rl_framework_example/futures_ctp/_main.py
+++ 6_rl_framework_example/futures_ctp/_main.py
@@ -2,21 +2,22 @@ import math
 import torch
 from rl_m19.config import Config
 from rl_m19.network import ReplayMemory, Nematode
-from rl_m19.utils import Plotter
+from rl_m19.utils import Logger, Plotter
 from dqn_agent_ext import DQNAgentExt
 from futures_ctp_env import FuturesCTP
 
 config = Config()
 config.episode_lifespan = 10**4
-config.episodes = 10**3
+config.episodes = 10**4
 config.BATCH_SIZE = 64
 config.GAMMA = 0.999
-config.EPS_fn = lambda s: 0.05 + (0.9 - 0.05) * math.exp(-1. * s / 1000)
-# config.EPS_fn = lambda s: 0.5
-config.LR = 0.001  # LEARNING_RATE
+# config.EPS_fn = lambda s: 0.9
+config.EPS_fn = lambda s: 0.01 + (0.99 - 0.01) * math.exp(-1. * s / 10000)
+config.LR = 0.01  # LEARNING_RATE
 config.MC = 1000  # MEMORY_CAPACITY
 config.TUF = 10  # TARGET_UPDATE_FREQUENCY
 
+config.logger = Logger()
 config.plotter = Plotter()
 config.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 config.env = FuturesCTP(config.device, config.plotter)
